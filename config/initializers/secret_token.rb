386d169fad62c772e4dc2ba6f2e520530be8e485
JQL <superfusia@gmail.com>
2012-03-20 17:32:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '9d0216b593f87d714e736f05a320bfc3a1da9dba205a6f7acc0b15a4a44d61a06f23a9afaec0952ed671384c5a73ffaf5255fc906457ac4340daa48288d923b5'
